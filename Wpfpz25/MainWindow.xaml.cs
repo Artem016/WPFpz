45a6fd990e900770b332439c1697b56375a5e729
Artem016 <artemklemonow@gmail.com>
2023-03-04 12:58:24 +0500

    Новый функционал

--- Wpfpz25/MainWindow.xaml.cs
+++ Wpfpz25/MainWindow.xaml.cs
@@ -20,6 +20,10 @@ namespace Wpfpz25
     /// </summary>
     public partial class MainWindow : Window
     {
+        double velue1, velue2;
+        char action;
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +32,30 @@ namespace Wpfpz25
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             wrot.Text = "котик";
+        }
+
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+            wrot.Text += "7";
+        }
+
+        private void equel_Click(object sender, RoutedEventArgs e)
+        {
+            velue2 = double.Parse(wrot.Text);
+            if(action == '+')
+            {
+                wrot.Text = Convert.ToString(velue1 + velue2);
+            }
+        }
+
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            if(wrot.Text != null)
+            {
+                action = Convert.ToChar(plus.Content);
+                velue1 = double.Parse(wrot.Text);
+                wrot.Text = null;
+            }
         }
     }
 }
